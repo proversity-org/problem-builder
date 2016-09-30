cb4e3be5fa567433da2218fa0fd4693ad946c9c8
jagonzalr <antonio@proversity.org>
2016-09-30 14:10:58 +0200

    innerHTML

--- problem_builder/public/js/review_blocks.js
+++ problem_builder/public/js/review_blocks.js
@@ -55,13 +55,14 @@ function ExportBase(runtime, element, initData) {
             var wrapperHTML = reportTemplate.replace('REPORT_GOES_HERE', $report.html());
             console.log($.parseHTML(wrapperHTML, document));
             console.log($.parseHTML(wrapperHTML, document)[9]);
-            console.log(typeof $.parseHTML(wrapperHTML, document)[9]);
-            console.log($report);
-            console.log(typeof $report);
-            console.log($report.context);
-            console.log(typeof $report.context);
+            // console.log(typeof $.parseHTML(wrapperHTML, document)[9]);
+            // console.log($report);
+            // console.log(typeof $report);
+            // console.log($report.context);
+            // console.log(typeof $report.context);
+            console.log($.parseHTML($.parseHTML(wrapperHTML, document)[9].innerHTML));
             var doc = new jsPDF(); 
-            doc.fromHTML($.parseHTML($.parseHTML(wrapperHTML, document)[9]).context, 15, 15, {
+            doc.fromHTML($.parseHTML($.parseHTML(wrapperHTML, document)[9].innerHTML).context, 15, 15, {
                 'width': 170
             });
 
