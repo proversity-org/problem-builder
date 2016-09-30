69225b6e5227f6be1c3974c31ccd2c7f823eabc7
jagonzalr <antonio@proversity.org>
2016-09-30 09:50:39 +0200

    change format of output in html

--- problem_builder/public/js/review_blocks.js
+++ problem_builder/public/js/review_blocks.js
@@ -58,9 +58,9 @@ function ExportBase(runtime, element, initData) {
             //     $(this).attr('href', string);
             //     // $('.preview-pane').attr('src', string);
             // });
-            var dataURI = "data:text/html;base64," + unicodeStringToBase64(wrapperHTML);
-            // var dataURI = "data:application/pdf;base64," + unicodeStringToBase64(wrapperHTML);
-            
+            // var dataURI = "data:text/html;base64," + unicodeStringToBase64(wrapperHTML);
+            var dataURI = "data:application/pdf;base64," + unicodeStringToBase64(wrapperHTML);
+            console.log(dataURI);
             // pdf.save('starapp' + '.pdf');
             $(this).attr('href', dataURI);
         }
