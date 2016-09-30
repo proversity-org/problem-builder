646a60c6ab5e73da89512858daf6b20afe0f39de
jagonzalr <antonio@proversity.org>
2016-09-30 11:48:05 +0200

    use the context of the element

--- problem_builder/public/js/review_blocks.js
+++ problem_builder/public/js/review_blocks.js
@@ -78,9 +78,7 @@ function ExportBase(runtime, element, initData) {
     function getCanvas(html){
      var a4  =[ 595.28,  841.89];
      html.width((a4[0]*1.33333) -80).css('max-width','none');
-     console.log(html.context);
-     console.log(html.context.nodeType);
-     return html2canvas(html,{
+     return html2canvas(html.context,{
          imageTimeout:2000,
          removeContainer:true
         }); 
